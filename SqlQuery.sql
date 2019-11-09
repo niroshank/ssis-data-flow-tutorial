9b5ca48bf992a59b3388d86835d5f5be827a25b3
Niroshan Kumarasinghe <niroshanku@jkintranet.com>
2019-11-09 13:15:14 +0530

    Create first ssis solution

---
+++ SqlQuery.sql
@@ -0,0 +1,5 @@
+﻿--Create new tables
+CREATE TABLE Emp2( Id int not null primary key, FullName varchar(255), LastName varchar(255), Address varchar(255),Phone int);
+CREATE TABLE Emp1( Id int not null primary key, Name varchar(255), Address varchar(255));
+CREATE TABLE Emp3( Id int not null primary key, FullName varchar(255), LastName varchar(255), Address varchar(255), Phone int);
+

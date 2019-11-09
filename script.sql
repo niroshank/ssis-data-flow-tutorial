20e7c6bc88a0d1d598bb1e9c96bb08f7cc5f22be
Niroshan Kumarasinghe <niroshanku@jkintranet.com>
2019-11-09 15:56:39 +0530

    Create text extraction data flow

---
+++ script.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Texts(Id int, Description varchar(255));
+
+INSERT INTO [dbo].[Texts]
+           ([Id]
+           ,[Description])
+     VALUES
+           (1, 'A book or other written or printed work, regarded in terms of its content rather than its physical form.'),
+		   (2, 'A book or other written or printed work, regarded in terms of the content rather than its physical form.'),
+		   (3, 'A or or other written or the work, regarded in the of its content the than its physical form.')
+
+GO
+
+CREATE TABLE TermScore(Term varchar(255), Score int);

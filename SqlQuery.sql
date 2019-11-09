4a0488a58c08849e53a16b07d7cc65043215fd5b
Niroshan Kumarasinghe <niroshanku@jkintranet.com>
2019-11-09 14:13:15 +0530

    merge 3 tables using derived column

--- SqlQuery.sql
+++ SqlQuery.sql
@@ -2,4 +2,4 @@
 CREATE TABLE Emp2( Id int not null primary key, FullName varchar(255), LastName varchar(255), Address varchar(255),Phone int);
 CREATE TABLE Emp1( Id int not null primary key, Name varchar(255), Address varchar(255));
 CREATE TABLE Emp3( Id int not null primary key, FullName varchar(255), LastName varchar(255), Address varchar(255), Phone int);
-
+CREATE TABLE All_Employee( Id int, FullName varchar(255), LastName varchar(255), Address varchar(255), Phone int);
